f53a72fe1654cb10b7513f64e7b6d3f9013f96b8
Meikul <meikul.harris@gmail.com>
2015-08-08 00:23:05 -0600

    styling and logic
    Changed the name to Jabberscript. Changed fonts to roboto slab. Added
    title. Took out all unnecessary recognized words. Changed the way
    non-list object attributes are created. Cleaning up some artifacts of
    foreach loop structure.

--- compiler.js
+++ compiler.js
@@ -4,46 +4,41 @@ function compile(){
   console.log(input);
   var recognizedWords = [
     'if',
-    'is',
-    'are',
-    'while',
-    'or',
-    'and',
-    'a',
-    'an',
-    'list',
-    'plus',
-    'minus',
-    'divided',
-    'multiplied',
-    ];
-    var objects=[];
-    var lists=[];
-    var thingTypes=[];
+    'while'
+  ];
+  var objects=[];
+  var lists=[];
+  var thingTypes=[];
   var script = '';
   var compare = false;
   var nextWord;
   var prevWord;
   var word;
-  //input.forEach(function(word){
+  var exami;
+  var examWord;
+  var examString='';
   for(i=0;i<input.length;i++){
     word=input[i];
-    nextWord=input[i+1];
-    prevWord=input[i-1];
-    if(recognizedWords.indexOf(word)===-1){
+    if(isRecWord(word)){
       //not recognized
       if(word.substring(word.length-3,word.length-1)==="'s"||word.substring(word.length-3,word.length-1)==="s'"){
         //object
         script+=word+' = new Object();';
         objects.push(word);
         if(input[i+2]==='is'){//attribute is a single value
-          script+=word+'.'+nextWord+'='+input[i+3]+';';
-          i+=2;
-        }
+          exami=3;
+          examString='';
+          while(isRecWord(input[i+exami]) && senEnd(input[i+exami-1])){//creates a string until end of sentence or until trigger word.
+            examString+=input[i+exami];
+            exami++;
+          }
+          script+=word+'.'+nextWord+'="'+examString+'";';
+          i+=exami;
+          }
         else if (input[i+2]==='are'){//attribute is an array
           script+=word+'.'+nextWord+'=[';
-          var exami=3;
-          var examWord=input[i+exami];
+          exami=3;
+          examWord=input[i+exami];
           while (examWord!==='and'||examWord!==='or'){
             examWord=input[i+exami];
             if(examWord.substring(examWord.length-2,examWord.length-1)===','){//accounts for non-oxford commas
@@ -54,8 +49,8 @@ function compile(){
             exami++;
           }
           script+=input[i+exami+1]+'];';
-          i+=exami;
-        }
+          //skip
+          }
       }
       if(input[i+2]==='a'||input[i+2]==='an'){
         if(input[i+3]==='list'){//list data type
@@ -65,8 +60,8 @@ function compile(){
           script+=word+'={type:'+input[i+3]+'};';//new object with 'type' attribute
           thingTypes.push(input[i+3]);
           objects.push(word);
-          i+=2;
-        }
+          //skip
+          }
       }
     }
     else{//recognized
@@ -76,22 +71,24 @@ function compile(){
           if(input[i+2]==='is'){
             if(input[i+3]==='not'){
               script+='!';
-              i++;
+              //skip
+              }
+            if (input[i+3]==='less'){
+              script+='<';
+              if(input[i+5]==='or'&&input[i+6]==='equal'){
+                script+='=';
+                //skip
+                }
+            }else if (input[i+3]==='greater']){
+              script+='>';
+              if(input[i+5]==='or'&&input[i+6]==='equal'){
+                script+='=';
+                //skip
+                }
             }
           } else if(input[i+2]==="isn't"){
             script+='!';
-          } else{
-            error('"'+input[i+2]+'" at index '+i+' is not a valid if statment operation.');
           }
-          if (input[i+3]==='less'){
-            script+='<';
-          }else if (input[i+3]==='greater']){
-            script+='>'
-            if(input[i+5]==='or'&&input[i+6]==='equal'){
-              script+='=';
-            }
-          }
-          
           break;
         case 'while':
           break;
@@ -103,7 +100,6 @@ function compile(){
           }
           if(nextWord==='a'||nextWord==='an'){
             //var is data type
-
           }
           break;
         default:
@@ -113,6 +109,20 @@ function compile(){
     }
   }
   return script;
+}
+function isRecWord(w){
+  if(recognizedWords.indexOf(w)===-1){
+    return false;
+  }else{
+    return true;
+  }
+}
+function senEnd(w){
+  if(w.substring(w.length-2, w.length-1)==='.'){
+    return true;
+  }else{//if not the end of sentence then returns what last character is.
+    return w.substring(w.length-2, w.length-1);
+  }
 }
 function error(message){
   alert(message);
